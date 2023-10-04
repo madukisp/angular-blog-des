d38dfc5a4d82cfe9e87518b2da2c9788bac7e6cd
Amanda Duque <madukisp@gmail.com>
2023-10-03 23:58:19 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "QUEEN",
+        "description": "Freddie Mercury foi o icônico vocalista da banda Queen, celebrado por sua voz única, carisma e performances energéticas no palco.",
+        "photoCover":"https://i.pinimg.com/564x/2c/85/eb/2c85eb2bde44430b405dcbe3799e40c8.jpg"
+    },
+    {
+        "id":"2",
+        "title": "saiu nova música",
+        "description": "olá mundo!",
+        "photoCover":""
+    }
+]
